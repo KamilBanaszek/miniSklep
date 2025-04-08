f95714000f44c2c5b89751f8b31c52b35a94460f
Kamil Banaszek <banaszekkamil120@gmail.com>
2025-04-08 17:18:12 +0200

    Add ProductForm as a ModelForm for creating products

--- webApka/miniSklep/views.py
+++ webApka/miniSklep/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+        labels = {
+            'name': 'Nazwa produktu',
+            'price': 'Cena (PLN)'
+        }
