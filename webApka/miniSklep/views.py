12ffc732acffcf6a002b95b15ce908ac628ae008
Kamil Banaszek <banaszekkamil120@gmail.com>
2025-04-08 17:28:30 +0200

    Create user_login view

--- webApka/miniSklep/views.py
+++ webApka/miniSklep/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 from .models import Product
 
 class ProductForm(forms.ModelForm):
@@ -9,3 +11,19 @@ class ProductForm(forms.ModelForm):
             'name': 'Nazwa produktu',
             'price': 'Cena (PLN)'
         }
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('list')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user.is_superuser:
+                login(request, user)
+                return redirect('list')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
